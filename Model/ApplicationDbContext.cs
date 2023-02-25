99bb7838b50d2a301e6bb62f91ab31b15b59dc5d
TEDLIUCOMPUTER\asus <qsc811022@gmail.com>
2023-02-25 11:54:06 +0800

    AD Dbconetext

---
+++ Model/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookListRazor_ted.Model
+{
+    public class ApplicationDbContext
+    {
+    }
+}
