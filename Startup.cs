6844d1f38d73f7303021e212e46ef218343387b8
TEDLIUCOMPUTER\asus <qsc811022@gmail.com>
2023-02-25 11:34:04 +0800

    add NewNuGet

--- Startup.cs
+++ Startup.cs
@@ -4,6 +4,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.Mvc.Razor.RuntimeCompilation;
+
 
 using System;
 using System.Collections.Generic;
@@ -24,7 +26,7 @@ namespace BookListRazor_ted
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddRazorPages();
+            services.AddRazorPages().AddRazorRuntimeCompilation();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
