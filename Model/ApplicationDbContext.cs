e7dd2eef7c639d19a9afe397e23e3e67fb5b46de
TEDLIUCOMPUTER\asus <qsc811022@gmail.com>
2023-02-25 17:47:20 +0800

    add DbContext

--- Model/ApplicationDbContext.cs
+++ Model/ApplicationDbContext.cs
@@ -1,11 +1,17 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace BookListRazor_ted.Model
 {
-    public class ApplicationDbContext
+    public class ApplicationDbContext:DbContext
     {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options)
+        {
+
+        }
     }
 }
