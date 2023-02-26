72def3a668b28581296e14944e910f4b7f4769e5
TEDLIUCOMPUTER\asus <qsc811022@gmail.com>
2023-02-26 11:18:48 +0800

    Add Dbcontext

--- Startup.cs
+++ Startup.cs
@@ -11,6 +11,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BookListRazor_ted.Model;
+using Microsoft.EntityFrameworkCore;
 
 namespace BookListRazor_ted
 {
@@ -26,6 +28,7 @@ namespace BookListRazor_ted
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<ApplicationDbContext>(option=>option.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
             services.AddRazorPages().AddRazorRuntimeCompilation();
         }
 
