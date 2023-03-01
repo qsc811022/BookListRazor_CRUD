9c89b6d9a107627af72228e06d48cfd201f8a6db
TEDLIUCOMPUTER\asus <qsc811022@gmail.com>
2023-03-01 22:29:24 +0800

    47

---
+++ wwwroot/js/BookListJavaScript.js
@@ -0,0 +1,19 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('BookListJavaScript', BookListJavaScript);
+
+    BookListJavaScript.$inject = ['$location'];
+
+    function BookListJavaScript($location) {
+        /* jshint validthis:true */
+        var vm = this;
+        vm.title = 'BookListJavaScript';
+
+        activate();
+
+        function activate() { }
+    }
+})();
